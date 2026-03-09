516535a543d9e36e6a75c88a7c2bb19e5191b770
Ruben <125226745+ayeeeruben@users.noreply.github.com>
2026-03-09 02:56:14 +0000

    fixed dev issues and reverted my changes with axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
-import "./index.css"
-import { CalendarApp } from "./components/calendar-app";
-function App() {
-  return <CalendarApp />
-}
-
-export default App
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
 
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import { CalendarApp } from "./components/calendar-app";
+
+import "./index.css";
+import "./App.css";
 
 export default function App() {
   return (
@@ -17,6 +13,10 @@ export default function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      {/* your calendar page */}
+      <Route path="/calendar" element={<CalendarApp />} />
+
       <Route path="*" element={<div style={{ padding: 24 }}>Not found</div>} />
     </Routes>
   );
